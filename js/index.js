d7d4c6f0bb1e72a1397074b1734d35d46d039fe4
phewson16 <peterhewson16@gmail.com>
2024-11-04 10:29:46 -0500

    temp push, finishing now

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
+const gratInput = document.getElementById('userInput');
+const gratitudeList = document.getElementById('gratList');
+
+
 function addGratitude() {
+    console.log('grat func');
+
+    // read text input
+
+
+
+    // const sweetItem = document.createElement("li");
+    //sweetItem.textContent = sweetRecipe[i].name;    
+    //sweetItem.appendChild(link);
+    //sweetItem.className = 'sweet';
+    //recipeDisplay.appendChild(sweetItem);
+
     
 }
